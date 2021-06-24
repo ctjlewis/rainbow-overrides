0bf84d3d1511bd34c483026f98232a31504ce514
C. Lewis <ctj.lewis@icloud.com>
2021-06-24 18:32:04 -0500

    chore: build `dist/` each run

--- dist/write.js
+++
@@ -1,6 +0,0 @@
-import { promises as fs } from 'fs';
-import { getOverrides } from './compile.js';
-export const write = async () => {
-    const overrides = await getOverrides();
-    await fs.writeFile('rainbow-token-overrides.json', JSON.stringify(overrides));
-};
