10947de9d6b091cafff02844c0d41ef3758aa555
C. Lewis <ctj.lewis@icloud.com>
2021-06-26 17:29:27 -0500

    chore: trigger Token List rebuild on push

---
+++ scripts/trigger-rebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPO="ctjlewis/rainbow-token-list"
+
+curl -XPOST \
+  -u "${{ github.actor }}:${{secrets.GITHUB_TOKEN}}" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Content-Type: application/json" \
+  https://api.github.com/repos/$REPO/actions/workflows/write.yaml/dispatches \
+  -d '{"ref": "ref"}'
