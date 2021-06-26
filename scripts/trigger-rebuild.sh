d6e589fcd8edd531e497e3fae9a0c163808718c1
C. Lewis <ctj.lewis@icloud.com>
2021-06-26 17:48:04 -0500

    test: bearer token

--- scripts/trigger-rebuild.sh
+++ scripts/trigger-rebuild.sh
@@ -6,5 +6,5 @@ curl -XPOST \
   -u 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Content-Type: application/json" \
-  -d '{"ref": "ref"}' \
+  -d '{"ref": "service-compatibility"}' \
   https://api.github.com/repos/$REPO/actions/workflows/write.yaml/dispatches
