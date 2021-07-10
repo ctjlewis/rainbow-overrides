4dfc9265419d1c5469286cdbdfeb81d44d192b66
C. Lewis <ctj.lewis@icloud.com>
2021-07-10 01:58:18 -0500

    chore: compile with new TSDX

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,8 +1,8 @@
+#!/usr/bin/env node
+'use strict';
 
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./rainbow-overrides.production.min.cjs')
-} else {
-  module.exports = require('./rainbow-overrides.development.cjs')
-}
+const { NODE_ENV } = process.env;
+if (NODE_ENV === 'production')
+  module.exports = require('./rainbow-overrides.production.min.cjs');
+else
+  module.exports = require('./rainbow-overrides.development.cjs');
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,4 +1,4 @@
+#!/usr/bin/env node
 
 export { default } from './rainbow-overrides.min.mjs';
 export * from './rainbow-overrides.min.mjs';
-  
